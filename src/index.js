59edf5f7d07a20ad98a3fcc996614efae41db5dd
Rodrigo Garcia <rgs190385@gmail.com>
2019-12-19 22:22:59 -0300

    passando para o javascript

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const $root = document.querySelector("#root")
 const $memoryCard = document.createElement("article");
+const $memoryCardFront = document.createElement("article")
+
+
 const $icon =
     `<img src='img/icon-collabcode.png'
      alt='Gueio mascote da Collabcode'
-     class='icon'/>`
+     class='icon' />
+`;
+
+const $iconC =
+    `<img src='img/icon-c.png' 
+    alt='Icone de um livro da liguagem c++' 
+    class='icon'>
+`;
+
+
 
 $memoryCard.classList.add("memory-card")
-
-
-
 $root.insertBefore($memoryCard, null)
+
+$memoryCardFront.classList.add("memory-card")
+$memoryCardFront.classList.add("-front")
+$root.insertBefore($memoryCardFront, null)
+
 $memoryCard.insertAdjacentHTML('afterbegin', $icon);
+
+$memoryCardFront.insertAdjacentHTML('afterbegin', $iconC)
