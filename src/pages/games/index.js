c84fb9551029734c2cc6c9c20464755e9666fd0c
Rodrigo Garcia <rgs190385@gmail.com>
2020-01-05 23:18:44 -0300

    alguma correções nas paginas

--- src/pages/games/index.js
+++ src/pages/games/index.js
@@ -1,13 +1,14 @@
 const $root = document.querySelector("#root")
 const $memoryCardFront = document.createElement("article")
+
+
 createMemoryCard();
 
 const $iconC =
     `<img src='img/icon-c.png' 
-    alt='Icone de um livro da liguagem c++' 
-    class='icon'>
+    alt='Icone de um livro da liguagem c++'
+    class='icon'/>
 `;
-
 
 $memoryCardFront.classList.add("memory-card")
 $memoryCardFront.classList.add("-front")
