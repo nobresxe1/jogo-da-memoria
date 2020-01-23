a4c5e8804499f5ece8e18c494b1c308d764a9669
Rodrigo Garcia <rgs190385@gmail.com>
2020-01-22 21:00:50 -0300

    pequena refatoração usando if ternario

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,25 +1,20 @@
-const createMemoryCard = nameClass => {
-        let src = 'img/icon-collabcode.png';
-
-        if (nameClass == '-front') {
-            src = 'img/icon-c.png'
-        }
-
-        return `
+const createMemoryCard = nameClass => `
         <article class = "memory-card ${nameClass}">
-            <img src='${src}'
+            <img src='${ nameClass == '-front' ? 'img/icon-c.png':'img/icon-collabcode.png'}'
             alt='Gueio mascote da Collabcode'
             class='icon'
             onclick = 'handleClick()'/>
-        </article>`;
-    }
-    // const createMemoryCardFront = () => `
-    //     <article class = "memory-card -front">
-    //         <img src='img/icon-c.png' 
-    //         alt='Icone de um livro da liguagem c++'
-    //         class='icon'
-    //         onclick='handleClick()'/>
-    //     </article>`;
+        </article>
+        `
+
+// const createMemoryCardFront = () => `
+//     <article class = "memory-card -front">
+//         <img src='img/icon-c.png' 
+//         alt='Icone de um livro da liguagem c++'
+//         class='icon'
+//         onclick='handleClick()'/>
+//     </article>`;
+// foi usado IF ternario
 
 
 
