3257bf83a7222c1782784977685d4155cf10cd78
Rodrigo Garcia <rgs190385@gmail.com>
2020-01-23 21:15:27 -0300

    executado o exercício de criar mais cards

--- src/pages/games/index.js
+++ src/pages/games/index.js
@@ -2,16 +2,21 @@ const $root = document.querySelector("#root")
 
 const $cardsWrapper = createCardsWrapper();
 const $memoryCard = createMemoryCard("img/icon-collabcode.png", "Gueio mascote da Collabcode");
-const $memoryCardFront = createMemoryCard("img/icon-c.png", "Icone de um livro da liguagem c++", "-front");
+const $memoryCardC = createMemoryCard("img/icon-c.png", "Icone de um livro da liguagem c++", "-front");
+const $memoryCardBug = createMemoryCard("img/icon-bug.png", "Icone do bug", "-front");
+const $memoryCardCelular = createMemoryCard("img/icon-celular.png", "Icone do celular", "-front");
+const $memoryCardSettings = createMemoryCard("img/icon-settings.png", "icone de configuração", "-front")
 
 $cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardC);
 $cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardBug)
 $cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardCelular);
 $cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardFront);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardSettings);
+
+
 
 
 
